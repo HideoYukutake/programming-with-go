fc07cd0486ae4885865469526d5cb0cfaabebec0
Hideo Yukutake <stabilite-animee@live.jp>
2019-09-06 22:02:45 +0900

    Add chapter01 answer

---
+++ chapter01.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Print my name.
+	fmt.Printf("Hello, Hideo!\n")
+	// Print hello world.
+	fmt.Printf("Hello, world\n")
+	// Print world in Japanese.
+	fmt.Printf("Hello, 世界\n")
+}
